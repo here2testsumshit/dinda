ad0b156312aa6141baf42714b16834efb39be1ba
here2testsumshit <lazyaqshasp@gmail.com>
2024-11-25 23:55:25 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Surprise Farah!').split('')
+    const titles = ('yang semangat ya mely!').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
