8cc7f2d28118fb9a05848015d902adcfe26d5b6e
here2testsumshit <lazyaqshasp@gmail.com>
2025-02-02 01:04:19 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'aku benar" beruntung banget ketemu denganmu!'.split('')
+const text = 'aku benar" beruntung banget ketemu dengan kamu!'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
