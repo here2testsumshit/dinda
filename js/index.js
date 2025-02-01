ed273a3d37770e6733b2d0814435f49710864383
here2testsumshit <lazyaqshasp@gmail.com>
2025-02-02 00:57:37 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'you deserve all the love and care you need!'.split('')
+const text = 'aku benar" beruntung banget kenal denganmu!'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
